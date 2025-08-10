3b97c13b08bf04fc33fdf390c75f9e03dd64ebf7
suryansh9331 <suryanshmishra9331@gmail.com>
2025-08-10 23:59:09 +0530

    unnecessory code removed v3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 const express = require('express');
-const connectDB = require('./config/db'); // Make 
+const connectDB = require('./config/db'); 
 const usersRouter = require('./routes/users');
 const adminRouter = require('./routes/admin');
 const errorHandler = require('./middleware/errorHandler');
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-// Middleware to parse JSON requests
 app.use(express.json());
 
 // Root route
@@ -30,10 +29,10 @@ app.use((req, res, next) => {
   res.status(404).json({ message: 'Not Found' });
 });
 
-// Global error handler (must be after all routes)
+
 app.use(errorHandler);
 
-// Start server
+
 app.listen(PORT, () => {
   console.log(`✅ Server listening on port ${PORT}`);
 });
